ada3ba57ca326de697e57b7236efd4586ec3c1d2
Ashish <ashish.mi.mishra@oracle.com>
2023-02-03 02:27:32 +0530

    imgn AI contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,6 @@ require("@nomicfoundation/hardhat-toolbox");
 const ALCHEMY_API_KEY = "-u0CZe73uLcbrag3ydgz7H05oD57ma-Z";
 const GOERLI_PRIVATE_KEY = "e69680a36bbcbdc74ecc8afa4fc45ec74806a40b9e41dd2437812d8b5cffc456";
 
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
---
+++ scripts/deployImgnAI.js
@@ -0,0 +1,19 @@
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    const ImgnAI = await ethers.getContractFactory("ImgnAI");
+    const token = await ImgnAI.deploy();
+
+    console.log("ImgnAI Token address:", token.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
